3d69d34d91c48131ccad23d353a02bdecf2c46ab
Zack Guard <zacharyguard@gmail.com>
2014-11-14 04:03:06 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# hr-time-js
+
+Simple human-readable timestamps
+
+## Usage
+
+`new HRTime(Date then, [Object options])` returns a human-readable timestamp.
+
+### Options
+
+`options` can include the following keys:
+
++ __`Date now`__
+
+    Date to calculate time difference from.
+
+    _Default_: `new Date()`
+    
+
++ __`Boolean roundDown`__
+
+    Whether or not to round time differences down.
+    
+    _Default_: `false`
+
+### Example usage and output
+
+```javascript
+new HRTime(new Date("13 Nov 2014 20:05 GMT+0800"), {
+    now: new Date("13 Nov 2014 20:55 GMT+0800),
+    roundDown: true
+});
+  
+// returns
+{
+    time: 50
+    unit: "minute",
+}
+```
---
+++ hr-time.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var HRTime = function(then, options) {
+    
+    var SECOND = 1000,
+        MINUTE = SECOND * 60,
+        HOUR = MINUTE * 60,
+        DAY = HOUR * 24,
+        MONTH = DAY * 30.583, // mean month length
+        YEAR = MONTH * 12,
+        DECADE = YEAR * 10,
+        CENTURY = YEAR * 100,
+        MILLENIUM = YEAR * 1000;
+    
+    var now = new Date();
+    var roundFunc = Math.round;
+    
+    if (options) {
+        if (options.now) now = options.now;
+        if (options.roundDown) roundFunc = Math.floor;
+    }
+    
+    if (!(then.constructor === Date && now.constructor === Date)) {
+        return false;
+    }
+    
+    var delta = now.getTime() - then.getTime();
+    
+    var times = {
+        millisecond: roundFunc(delta),
+        second: roundFunc(delta / SECOND),
+        minute: roundFunc(delta / MINUTE),
+        hour: roundFunc(delta / HOUR),
+        day: roundFunc(delta / DAY),
+        month: roundFunc(delta / MONTH),
+        year: roundFunc(delta / YEAR),
+        decade: roundFunc(delta / DECADE),
+        century: roundFunc(delta / CENTURY),
+        millenium: roundFunc(delta / MILLENIUM)
+    };
+
+    var timesKeys = Object.keys(times);
+
+    var returnVal = {
+        time: times.millisecond,
+        unit: "millisecond"
+    };
+
+    timesKeys.forEach(function(key){
+        if (times[key] < returnVal.time && times[key] > 0) {
+            returnVal.time = times[key];
+            returnVal.unit = key;
+        }
+    });
+
+    return returnVal;
+};
